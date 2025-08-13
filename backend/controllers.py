142e46630785f65995bf09cffb16dcd285403aa6
Rodrigo <gomesrodrigo696@gmail.com>
2025-08-13 12:43:20 -0300

    inicio do endpoint cadastro_paciente

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from services import *
+from schemas import *
 
 app = FastAPI()
 
@@ -9,5 +9,53 @@ def read_root():
     return {"status": "ok"}
 
 @app.post("/cadastro_paciente")
-def cadastro_paciente(cadastro: cadastro_schema):
+def cadastro_paciente(cadastro:cadastro_schema):
+    #algumas informaçoes tiveram que ser colocadas em campos improvisados 
+    data = {
+    "name":cadastro.nome,
+    "birthName": None,
+    "flagWhatsapp": False,
+    "cns": "",
+    "sns": "",
+    "address":cadastro.bairro,
+    "number": "",
+    "rg": "",
+    "cpf": "",
+    "passport": "",
+    "passport_valid_date": "",
+    "apartment": "",
+    "neighborhood": "",
+    "city": "",
+    "state": "",
+    "zip": "",
+    "cellphone":cadastro.telefone,
+    "phone": "",
+    "email": "",
+    "obs":cadastro.diagnostico,
+    "sex": "",
+    "dateOfBirth": "15/06/1985",
+    "country": "BR",
+    "profession":cadastro.disciplina,  
+    "educationLevel": "",
+    "education": "",
+    "idHealthInsurance": "",
+    "healthProfessionalResponsible":cadastro.doutor,
+    "healthInsurancePlan": "",
+    "healthInsurancePlanCardNumber": "",
+    "indicatedBy": str({"hospital" :cadastro.hospital,"data" :cadastro.dataProcura}),
+    "genre": "",
+    "bloodType": "",
+    "bloodFactor": "",
+    "maritalStatus": "",
+    "religion": "",
+    "healthInsurancePlanCardNumberExpiry": "",
+    "kinships": [],
+    "responsibleName1": "",
+    "kinship": "",
+    "relationship": "",
+    "acceptDuplicate": False,
+    "acceptDuplicateCpf": False,
+    "acceptMinorPatient": False,
+    "cellphoneCountry": "BR"
+    }
     return {}
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,11 @@
-#widely used types go here
+from pydantic import BaseModel
+
+class cadastro_schema(BaseModel):
+    nome: str  
+    telefone: str         
+    bairro: str
+    diagnostico: str
+    disciplina: str
+    hospital: str
+    doutor: str
+    dataProcura: str
